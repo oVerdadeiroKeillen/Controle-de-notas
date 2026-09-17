f27e2c8c0532ea897d652ee5894d7fbe0c56cbd5
oVerdadeiroKeillen <keillenverdade@gmail.com>
2026-09-17 13:18:18 +0100

    Criar main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var alunoNome = getElementById("AlunoNome")
+var alunoNum = getElementById("AlunoNum")
+var salaNum = getElementById("salaNum")
+var turma = getElementById("turma")
+var notas = []
+let c = 1
+for (c;c<=alunosN;c++) {
+  
+}
+console.log(notas)
